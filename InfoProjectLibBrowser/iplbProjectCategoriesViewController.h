3e0d9ff61d6a64c99c04e84e6e744ee5b9639c00
xuanyuanzhiyuan <xuanyuanzhiyuan@gmail.com>
2014-03-22 17:13:40 +0800

    add categories feature in app.

---
+++ InfoProjectLibBrowser/iplbProjectCategoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  iplbProjectCategoriesViewController.h
+//  InfoProjectLibBrowser
+//
+//  Created by jinyanhua on 14-3-22.
+//  Copyright (c) 2014年 com.xysoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iplbProjectCategoriesViewController : UIViewController
+
+@end
---
+++ InfoProjectLibBrowser/iplbProjectCategoriesViewController.m
@@ -0,0 +1,38 @@
+//
+//  iplbProjectCategoriesViewController.m
+//  InfoProjectLibBrowser
+//
+//  Created by jinyanhua on 14-3-22.
+//  Copyright (c) 2014年 com.xysoft. All rights reserved.
+//
+
+#import "iplbProjectCategoriesViewController.h"
+
+@interface iplbProjectCategoriesViewController ()
+
+@end
+
+@implementation iplbProjectCategoriesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
